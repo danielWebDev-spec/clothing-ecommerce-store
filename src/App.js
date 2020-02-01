c10e691bc1b4312d48cf0f69ca57ed7fece40b99
deelocruz <kddaniel8000@gmail.com>
2020-02-01 11:50:27 -0500

    adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage'
 
-class App extends React.Component {
-  // constructor(){
-  //   super();
-  //   this.state = {
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
-  //   }
-  // }
+class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <HomePage />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/hats' component={HatsPage} />
+        </Switch>
       </div>
     );
   }
