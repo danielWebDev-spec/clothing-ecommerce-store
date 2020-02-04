79a65bcf2f866416d42e7179956dcc6570a89495
deelocruz <kddaniel8000@gmail.com>
2020-02-04 15:54:49 -0500

    adding auth, lifecycle methods, and unsubscribeFrom auth to App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,41 @@ import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
 import SignInSignUpPage from './pages/SignInSignUpPage/SignInSignUpPage';
 import Header from './components/Header/Header';
+import { auth } from './firebase/firebase.utils';
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-      <Route path='/signin' component={SignInSignUpPage} />
-    </Switch>
-  </div>
-);
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => 
+      this.setState({ currentUser: user })
+    )
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return(
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUpPage} />
+      </Switch>
+    </div>
+    )
+  } 
+}
   
 export default App;
