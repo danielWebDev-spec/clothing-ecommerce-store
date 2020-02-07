bdaa6e7f7a3c5d85c01edbc670cce1439cac28d1
deelocruz <kddaniel8000@gmail.com>
2020-02-07 14:50:20 -0500

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CollectionPreview from '../../components/CollectionPreview/CollectionPreview';
-import { SHOP_DATA } from '../../components/Data/shopData';
+import SHOP_DATA from '../../components/Data/shopData';
 
 class ShopPage extends React.Component {
     constructor(props){
