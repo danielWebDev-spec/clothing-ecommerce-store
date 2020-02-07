1c98d4c8d9f32a569a85735aeb678e57ab6a7bc6
deelocruz <kddaniel8000@gmail.com>
2020-02-07 00:26:48 -0500

    CartIcon component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
 
 import { auth } from '../../firebase/firebase.utils';
-import { connect } from "react-redux";
+import CartIcon from '../CartIcon/CartIcon';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
